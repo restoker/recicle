0e090763bb657386d8e78ccae77ab482604f6ca2
restoker <milthon.doom@gmail.com>
2025-07-22 19:34:38 -0500

    url server fixed

--- src/server/actions/tokens.ts
+++ src/server/actions/tokens.ts
@@ -2,8 +2,20 @@
 
 export const confirmToken = async (token: string) => {
     try {
-        const resp = await fetch(`http://localhost:3001/users/verification?token=${token}`);
+        const resp = await fetch(`http://localhost:3001/users/verification?token=${token}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
         const data = await resp.json();
+        if (data.statusCode) {
+            if (data.message === 'Validation failed (uuid is expected)') {
+                return { ok: false, msg: 'El formato del token no es valido' };
+            };
+
+            return { ok: false, msg: data.message };
+        }
         if (data.ok) {
             return { ok: true, msg: data.msg };
         }
